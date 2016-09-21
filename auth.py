42a9b0c770788a4746ee13a944ccc1440cf26183
tangzhiyi <854742740@qq.com>
2016-09-21 14:39:55 +0800

    modify class design

--- auth.py
+++ auth.py
@@ -9,6 +9,7 @@ import os
 import sys
 import termcolor
 import ConfigParser
+import pickle
 
 from bs4 import BeautifulSoup
 
@@ -16,15 +17,13 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 
-class Douban(object):
-    domain = 'https://www.douban.com/group/xiaotanzi/'
+class Auth(object):
+    domain = 'https://www.douban.com/'
 
     def __init__(self, account, password):
         self.account = account
         self.password = password
         self.login_url = 'https://www.douban.com/accounts/login?source=group'
-
-        self.jar = cookielib.LWPCookieJar('cookie')
         self.form = {
             'form_email' : self.account,
             'form_password' : self.password,
@@ -40,14 +39,14 @@ class Douban(object):
             'Accept-Language': 'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
             'Accept-Encoding' : 'gzip, deflate, br',
         }
+        self.jar = cookielib.CookieJar()
         self.session = requests.Session()
-        self.session.cookies = cookielib.LWPCookieJar('cookies')
-        self.response = self.session.get(self.login_url, cookies=self.jar, headers=self.headers, verify=False)
+        #self.response = self.session.get(self.login_url, cookies=self.session.cookies, headers=self.headers, verify=False)
 
     def login_douban(self, redir=domain):
 
         self.form['redir'] = redir
-        self.response = self.session.post(self.login_url, data=self.form, headers=self.headers)
+        self.response = self.session.post(self.login_url, cookies=self.jar, data=self.form, headers=self.headers)
         print 'Posted form...'
 
         #handle captcha code
@@ -58,9 +57,9 @@ class Douban(object):
                 self.captcha_handle()
             else:
                 break
-        self.session.cookies.save()
+        print self.session.cookies
         #write response into file , just for test
-        self.test_result()
+        self.test_result(self.response)
         print 'login successful!'
 
     def captcha_handle(self):
@@ -87,16 +86,35 @@ class Douban(object):
         else:
             return (None, None)
 
-    def test_result(self):
+    def test_result(self, result):
         with open('result.txt', 'w') as result_file:
-            result_file.write(self.response.text)
+            result_file.write(result.text)
+
+    def is_login(self):
+        # check session
+        url = "https://www.douban.com/accounts"
+        r = self.session.get(url, allow_redirects=False, verify=False, headers=self.headers)
+        status_code = int(r.status_code)
+        print r.status_code
+        self.test_result(r)
+        if status_code == 301 or status_code == 302:
+            # 未登录
+            return False
+        elif status_code == 200:
+            print 'login already!'
+            return True
+        else:
+            print 'not login!'
+            return None
+
 
 
 if __name__ == "__main__":
-    (account, password) = Douban.parse_conf()
+    (account, password) = Auth.parse_conf()
     if account and password:
-        login = Douban(account, password)
+        login = Auth(account, password)
         login.login_douban()
+        login.is_login()
     else:
         print account
         print password
