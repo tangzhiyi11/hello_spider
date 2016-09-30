610c464b891583a889d0dcd125f91a81b31f3238
tangzhiyi11 <854742740@qq.com>
2016-09-30 11:31:13 +0800

    modify spider freq

--- hello.py
+++ hello.py
@@ -113,7 +113,7 @@ class douban_spider:
 
 def get_html_increase(url_list):
     for url in url_list:
-        i = 50000
+        i = 78000
         while i <= 359050:
             this_url = url + str(i)
             i += 25
